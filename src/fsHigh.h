923e655045a60505b5001e373e73eb22b5ee3174
Ichaudry <ichaudry@mail.sfsu.edu>
2020-04-17 22:28:22 -0700

    Bitmap initialization working without errors. TODO: Write root directory.

--- src/fsHigh.h
+++ src/fsHigh.h
@@ -1,12 +1,18 @@
 #ifndef FS_HIGH
 #define FS_HIGH
 
+//A=array
+//k=element in array
+#define SetBit(A,k)     ( A[(k/32)] |= (1 << (k%32)) )
+#define ClearBit(A,k)   ( A[(k/32)] &= ~(1 << (k%32)) )
+#define TestBit(A,k)    ( A[(k/32)] & (1 << (k%32)) )
 
 int startFileSystem(char * volName, uint64_t * volSize, uint64_t * blockSize,int format);
 void * printVolInfo();
-void * occupyMemoryBits(void * bitMap,unsigned long long startPosition, unsigned long long count);
-void * freeMemoryBits(void * bitMap,unsigned long long startPosition, unsigned long long count );
-void * count(void * bitMap,unsigned long long startPosition, unsigned long long count );
+void * getBitMap();
+void * occupyMemoryBits(int * bitMap,unsigned long long startPosition, unsigned long long count);
+void * freeMemoryBits(int * bitMap,unsigned long long startPosition, unsigned long long count );
+void * count(int * bitMap,unsigned long long startPosition, unsigned long long count );
 
 
 #endif
